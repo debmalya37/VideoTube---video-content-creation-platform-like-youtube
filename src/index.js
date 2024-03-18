9c641c3134c11eac77b7f7dfa77106801f63e0aa
Debmalya Sen <debmalyasen37@gmail.com>
2024-03-18 14:36:37 +0530

    #connected database in backend with mongodb mongoose, imported cors, dotenv, express in dependency, env file setup , constants.js and index.js code

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { Mongoose } from "mongoose";
+import {DB_NAME} from "./constants";
+
+
+// express import for initializing the app
+
+import express from "express";
+
+// initialising the app with express
+const app = express();
+
+
+
+// async await professional approach to connect database and error handling 
+// try catch method used to catch and throw the error occured in the process of connecting a database
+
+;( async () => {
+    try {
+        await Mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+    // app.on() is a listener function that is inbuilt in express,
+    // it works to listen any event
+    // here it is used to listen the error event to prevent having unknown error
+        app.on("error", ()=> {
+            console.log("ERROR:", error);
+            throw error;
+        })
+
+    // app.listen() shows the port where the database is running on
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App os listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+} )()
