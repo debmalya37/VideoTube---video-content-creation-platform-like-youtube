1244682a1c4c4d9be5359cf88ea0b7f1f1850eec
Debmalya Sen <debmalyasen37@gmail.com>
2024-04-10 15:37:55 +0530

    user.models.js done for now for database modelling

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,56 @@
 import  Mongoose, { Schema } from "mongoose";
 
-const userSchema = new Mongoose.Schema({});
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+            
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required !!']
 
-export User = Mongoose.model("User", userSchema);
+        },
+        refreshToken: {
+            type: String,
+
+        }
+},
+{
+    timestamps: true,
+    
+}
+);
+
+export const User = Mongoose.model("User", userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -18,4 +18,5 @@ export {asyncHandler};
 //             message: err.message
 //         })
 //     }
-// }
+// } 
+
